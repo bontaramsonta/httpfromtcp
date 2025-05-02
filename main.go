a9a33ad1da23cece90f9b6c5072bb1458b2207c6
souravdhar <bontaramsonta@gmail.com>
2025-05-02 21:56:08 +0530

    use goroutines and channels

--- main.go
+++ main.go
@@ -1,11 +1,43 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"os"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+	currentLine := ""
+
+	// read all bytes from file: 8 bytes at a time
+	go func() {
+		for {
+			b := make([]byte, 8, 8)
+			_, err := f.Read(b)
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				log.Fatal("Error reading file:", err)
+			}
+
+			for _, char := range string(b) {
+				if char == '\n' {
+					lines <- currentLine
+					currentLine = ""
+				} else {
+					currentLine += string(char)
+				}
+			}
+		}
+		close(lines)
+	}()
+
+	return lines
+}
 
 func main() {
 	file, err := os.Open("./messages.txt")
@@ -14,32 +46,10 @@ func main() {
 	}
 	defer file.Close()
 
-	var lines []string
-	currentLine := ""
+	lines := getLinesChannel(file)
 
-	// read all bytes from file: 8 bytes at a time
-	for {
-		b := make([]byte, 8, 8)
-		_, err := file.Read(b)
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			log.Fatal("Error reading file:", err)
-		}
-
-		for _, char := range string(b) {
-			if char == '\n' {
-				lines = append(lines, currentLine)
-				currentLine = ""
-			} else {
-				currentLine += string(char)
-			}
-		}
-	}
-
-	//! debug
-	for _, line := range lines {
+	for line := range lines {
 		fmt.Printf("read: %s\n", line)
 	}
+
 }
